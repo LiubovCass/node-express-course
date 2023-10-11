5f5aa7f6b1b61c8b5a62f3bb9544677847deabc2
Liubov Rodin <lyubov.lysenko@gmail.com>
2023-10-10 23:06:27 -0400

    added absolute path into sync.js

--- 01-node-tutorial/answers/fs-sync.js
+++ 01-node-tutorial/answers/fs-sync.js
@@ -1,12 +1,18 @@
 const { readFileSync, writeFileSync } = require('fs');
+const path = require('path');
 
-const first = readFileSync('../content/first.txt', 'utf8');
-const second = readFileSync('../content/second.txt', 'utf8');
-
-writeFileSync(
-  './temporary/fileA.txt',
-  { flag: 'a' },
-  `Here is the result: ${first}, ${second}`,
-  { flag: 'a' }
-);
-console.log(first, second);
+// const first = readFileSync('../content/first.txt', 'utf8');
+// const second = readFileSync('../content/second.txt', 'utf8');
+const line1 = 'I';
+const line2 = 'love';
+const line3 = 'NODEJS';
+// Get the absolute path to the file using __dirname
+const filePath = path.join(__dirname, 'temporary', 'fileA.txt');
+writeFileSync(filePath, line1, { flag: 'w' });
+console.log('first line!');
+writeFileSync(filePath, line2, { flag: 'a' });
+console.log('second line!');
+writeFileSync(filePath, line3, { flag: 'a' });
+console.log('third line!');
+readFileSync(filePath, 'utf8');
+console.log(line1, line2, line3);
