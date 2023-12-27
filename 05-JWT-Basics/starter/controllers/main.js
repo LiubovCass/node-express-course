2acb518531286bcac36d43eca76cf25eea09106e
Liubov Rodin <lyubov.lysenko@gmail.com>
2023-12-27 11:25:27 -0500

    answers for lesson 8

---
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -0,0 +1,44 @@
+//check username, password in post(login) request
+//if exist create new JWT
+//send back to front end
+
+//setup uuthentication so only the request with JWT can access the dashboard
+const jwt = require('jsonwebtoken');
+const { BadRequestError } = require('../errors'); //added {}
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  // console.log(username, password);
+  // res.send('Fake Login/Register/Signup Route');
+
+  //mongoose validation
+  //Joi
+  //check in the controller
+
+  if (!username || !password) {
+    throw new BadRequestError('Please provide email and password');
+  }
+
+  const id = new Date().getDate();
+
+  //try to keep payload small, better experience for user
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+  //   console.log(username, password);
+  res.status(200).json({ msg: 'user created', token });
+};
+
+const dashboard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100);
+
+  res.status(200).json({
+    msg: `Hello, ${req.user.username}`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  });
+};
+
+module.exports = {
+  login,
+  dashboard,
+};
